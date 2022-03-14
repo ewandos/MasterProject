1fb8bc8163ce5bf09929cdb67be4cb7139714f8a
StephanSte <44472829+StephanSte@users.noreply.github.com>
2022-03-14 17:38:39 +0100

    I made a gun that does the shoot
    ich hoff das das nicht schon was breaked

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField]
+    private float damage = 10f;
+    [SerializeField]
+    private float range = 100f;
+    [SerializeField] 
+    private float knockback = 30f;
+    [SerializeField] 
+    private float firerate = 15f;
+
+    [SerializeField] private float maxAmunition = 10f;
+    [SerializeField] private float amunition = 10f;
+    [SerializeField] private float amunitionCarried = 100f;
+    
+    
+    
+    [SerializeField]
+    private Camera fpsCam;
+    [SerializeField]
+    private ParticleSystem muzzleFlash;
+    [SerializeField] 
+    private GameObject impactEffect;
+
+
+    [SerializeField] private float nexTimeToFire = 0f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") 
+            && Time.time >= nexTimeToFire
+            && amunition > 0)
+        {
+            nexTimeToFire = Time.time + 1f / firerate;
+            Shoot();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reload();
+        }
+    }
+
+    void AddAmmo(float amount)
+    {
+        amunitionCarried += amount;
+    }
+    void Reload()
+    {
+        amunitionCarried -= (maxAmunition - amunition);
+        amunition = maxAmunition;
+    }
+    
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        amunition--;
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            HealthSystem target = hit.transform.GetComponent<HealthSystem>();
+
+            if (target != null)
+            {
+                //emit event
+                target.TakeDamage(damage);
+            }
+            
+            if (hit.rigidbody != null)
+            {
+                //emit event
+                hit.rigidbody.AddForce(-hit.normal * knockback);
+            }
+
+            GameObject impactGo =  Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGo, 1);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+public class HealthSystem : MonoBehaviour
+{
+    [SerializeField]
+    private float Health = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        Health -= amount;
+        if (Health <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
+    }
+}
