a56e2552f1b86b5ae8bf3d971193b234e548575e
Philip Ewert <developer@philipewert.com>
2022-05-27 15:25:43 +0200

    Added Quick Search Package +++ Pickups have sounds

--- Assets/HealthPickable.cs
+++ Assets/HealthPickable.cs
@@ -1,15 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthPickable : MonoBehaviour
 {
+    public GameObject model;
     public int amount = 10;
+
+    private AudioSource audio;
+
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         PlayerManager manager = other.GetComponent<PlayerManager>();
         if (manager == null) return;
         manager.health.AddHealth(amount);
-        Destroy(gameObject);
+        model.SetActive(false);
+        audio.Play();
+        Destroy(gameObject, 1);
     }
 }
--- Assets/MunitionPickable.cs
+++ Assets/MunitionPickable.cs
@@ -2,12 +2,22 @@ using UnityEngine;
 
 public class MunitionPickable : MonoBehaviour
 {
+    public GameObject model;
     public int amount = 5;
+    
+    private AudioSource audio;
+
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         PlayerManager manager = other.GetComponent<PlayerManager>();
         if (manager == null) return;
         manager.gun.AddAmmo(amount);
-        Destroy(gameObject);
+        model.SetActive(false);
+        audio.Play();
+        Destroy(gameObject, 1);
     }
 }
