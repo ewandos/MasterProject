134f2268dd11e39dbf890713049cd7878eeeec57
StephanSte <44472829+StephanSte@users.noreply.github.com>
2022-05-27 16:04:49 +0200

    WinScreen

---
+++ Assets/Scripts/Menu/WinScreen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+public class WinScreen : MonoBehaviour
+{
+    [SerializeField] private HealthSystem bosshealth;
+    [SerializeField] private GameObject endscreen;
+    
+    private bool skip = false;
+    public void FixedUpdate()
+    {
+        if (!skip && bosshealth.bossDead)
+        {
+            endscreen.SetActive(true);
+            Time.timeScale = 0;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            skip = true;
+        }
+    }
+
+}
