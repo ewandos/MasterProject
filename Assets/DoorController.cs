9396a23e259f026f296c54501fa0a40a2dca1676
Philip Ewert <developer@philipewert.com>
2022-06-02 14:34:50 +0200

    Door now can be locked with multiple keycards

--- Assets/DoorController.cs
+++ Assets/DoorController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,7 +8,8 @@ public class DoorController : MonoBehaviour
 {
     public bool startOpen;
     public bool openOnTriggerEnter;
-    public int code;
+    public List<int> codes = new List<int>();
+    public bool allCodesRequired;
     
     private Animator animator;
     private BoxCollider collider;
@@ -50,10 +52,22 @@ public class DoorController : MonoBehaviour
         }
 
         PlayerManager manager = other.GetComponent<PlayerManager>();
-        if (manager != null && manager.keychain.HasKeyFor(code))
-        {
+        if (manager != null)
+            EvaluateKeychain(manager);
+        
+    }
+
+    private void EvaluateKeychain(PlayerManager manager)
+    {
+        List<bool> foundCodeResults = new List<bool>();
+        foreach (int code in codes)
+            foundCodeResults.Add( manager.keychain.HasKeyFor(code));
+
+        if (allCodesRequired && !foundCodeResults.Contains(false))
             Open();
-        }
+        
+        if (!allCodesRequired && foundCodeResults.Contains(true))
+            Open();
     }
 
     private void OnTriggerExit(Collider other)
