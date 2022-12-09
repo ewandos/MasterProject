5956fbfcef5887429c21d4f2b9ebbbd3a4c69ce6
Philip Ewert <developer@philipewert.com>
2022-12-09 16:13:24 +0100

    Improvements
    - Shots dont hit trigger
    - Impact Effect does not Collide
    - Throw Glowsticks
    - Glowsticks dont collide with player and enemies

--- Assets/GlowstickController.cs
+++ Assets/GlowstickController.cs
@@ -4,16 +4,16 @@ public class GlowstickController : MonoBehaviour
 {
     
     public GameObject glowstickGO;
-
-
+    
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.G))
         {
             GameObject go = Instantiate(glowstickGO);
-            go.transform.position = transform.position + transform.forward + new Vector3(0, 0.5f);
+            go.transform.position = transform.position + new Vector3(0, 0.5f);
             go.transform.rotation = new Quaternion(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f), 0);
+            go.GetComponent<Rigidbody>().AddForce(transform.forward * 3, ForceMode.Impulse);
         }
     }
 }
