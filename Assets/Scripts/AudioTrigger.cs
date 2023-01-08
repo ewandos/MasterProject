d5053657d177ff0f533152ec54436be242bcc6a8
Philip Ewert <developer@philipewert.com>
2023-01-08 16:28:03 +0100

    Boss Room Trigger

--- Assets/Scripts/AudioTrigger.cs
+++ Assets/Scripts/AudioTrigger.cs
@@ -1,22 +1,28 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class AudioTrigger : MonoBehaviour
 {
+    public UnityEvent onTrigger;
     public AudioSource audioSource;
+    public List<AudioClip> randomAudioSources = new List<AudioClip>();
     public AudioClip audioClip;
     public float delay = 0f;
     public bool playOneShot;
 
     public void PlaySound()
     {
+        AudioClip clip = randomAudioSources.Count <= 0 ? audioClip : randomAudioSources[Random.Range(0, randomAudioSources.Count)] ;
+        
         if (playOneShot)
         {
-            audioSource.PlayOneShot(audioClip);
+            audioSource.PlayOneShot(clip);
             Destroy(gameObject);
         }
         else
         {
-            audioSource.clip = audioClip;
+            audioSource.clip = clip;
             audioSource.PlayDelayed(delay);
         }
         
@@ -26,5 +32,6 @@ public class AudioTrigger : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Invoke(nameof(PlaySound), delay);
+        onTrigger.Invoke();
     }
 }
