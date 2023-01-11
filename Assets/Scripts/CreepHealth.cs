cd7331796c7e041cf05ce9af8122f50dded96674
Philip Ewert <developer@philipewert.com>
2023-01-12 00:02:01 +0100

    Randomise Creep Health a bit

--- Assets/Scripts/CreepHealth.cs
+++ Assets/Scripts/CreepHealth.cs
@@ -1,8 +1,18 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CreepHealth : IHealth
 {
+    public int healthRange = 1;
     public GameObject corpseGO;
+
+    private void Start()
+    {
+        _maxHealth = Random.Range(_health - healthRange, _health + healthRange);
+        _health = _maxHealth;
+    }
+
     public override void Death()
     {
         Instantiate(corpseGO, transform.position, transform.rotation);
