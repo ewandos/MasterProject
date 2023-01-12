37f30ef09450834ad7bdaf21ad8748ce526b4575
Philip Ewert <developer@philipewert.com>
2023-01-12 15:50:32 +0100

    Improved Level Design

--- Assets/Scripts/Rotating.cs
+++ Assets/Scripts/Rotating.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 
 public class Rotating : MonoBehaviour
 {
+    public bool local = false;
     public float speed = 1f;
     private Vector3 initialPosition;
+    private Vector3 upVector;
 
     private void Start()
     {
         initialPosition = transform.position;
+        upVector = local ? transform.InverseTransformVector(Vector3.up) : Vector3.up;
     }
 
     void Update()
     {
-        transform.rotation = Quaternion.AngleAxis(speed * Time.deltaTime, Vector3.up) * transform.rotation;
+        transform.rotation = Quaternion.AngleAxis(speed * Time.deltaTime, upVector) * transform.rotation;
         transform.position = initialPosition + new Vector3(0, Mathf.Sin(Time.frameCount * 0.025f) * 0.15f, 0);
     }
 }
