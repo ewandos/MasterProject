e327b0c27815f7d348b10cc73137c9038e131818
Philip Ewert <developer@philipewert.com>
2023-01-12 22:18:25 +0100

    Credits Screen ++ QoL Changes

--- Assets/Scripts/GunPickable.cs
+++ Assets/Scripts/GunPickable.cs
@@ -4,9 +4,9 @@ public class GunPickable : Pickable
 {
     protected override bool OnPickUp(PlayerManager manager)
     {
+        manager.gun.enabled = true;
         GameState.PlayerPickedUpGunEvent?.Invoke(true);
         GameState.PlayerPickedUpItem?.Invoke(true);
-        manager.gun.enabled = true;
         return true;
     }
 }
