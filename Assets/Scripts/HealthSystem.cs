457241118992e877f648fe07abc7ed6fae312747
Philipp Andert <97304329+PhilippAndertMAI@users.noreply.github.com>
2023-01-18 12:45:44 +0100

    changes :)

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -7,69 +7,41 @@ using UnityEngine.SceneManagement;
  * THIS WILL BE REPLACED BY AN IMPLEMENTATION OF IHEALTH
  */
 
-public class HealthSystem : MonoBehaviour
+public class HealthSystem : IHealth
 {
-    [SerializeField]
-    private int health = 50;
-
-    [SerializeField] 
-    private int maxHealth = 50;
-
     [SerializeField] public bool bossDead = false;
 
     [SerializeField] private TextMeshProUGUI text;
-    [SerializeField] private bool player;
     
     public void Start()
     {
-        health = maxHealth;
-        if (GetComponent<PlayerMovement>())
-        {
-            player = true;
-            text.text = health.ToString();
-        }
+        _health = _maxHealth;
     }
 
     [Button]
     public void TakeDamage(int amount)
     {
-        health -= amount;
-        if (player)
-        {
-            text.text = health.ToString();
-        }
+		_health -= amount;
         
-        if (health <= 0)
+        if (_health <= 0)
         {
-            if (player)
-            {
-                text.text = "0";
-            }
             Death();
         }
     }
 
-    void Death()
+    public override void Death()
     {
-        if (player)
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-        }
-        else if (GetComponent<GOAPPlanner>())
-        {
-            //enable you win screen
-            bossDead = true;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+		bossDead = true;
+
+		Destroy(gameObject);
+
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
     public void AddHealth(int amount)
     {
-        health += amount;
-        if (health > maxHealth)
-            health = maxHealth;
+		_health += amount;
+        if (_health > _maxHealth)
+			_health = _maxHealth;
     }
 }
